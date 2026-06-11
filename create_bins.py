2728fabb04031a7bda9d8866867355bab3f2826f
talya toledano <talyatoledano10@gmail.com>
2026-06-11 13:56:44 +0300

    Add bin creation script

---
+++ create_bins.py
@@ -0,0 +1,16 @@
+import httpx
+
+KEY = "$2a$10$KoACzRcY64gqyT2LnOQ9UOG06ZE8gub8FZLzOm3B5nwUxz7mDEN92"
+
+for name in ["oren-ratings", "oren-feedback"]:
+    r = httpx.post(
+        "https://api.jsonbin.io/v3/b",
+        headers={
+            "X-Master-Key": KEY,
+            "Content-Type": "application/json",
+            "X-Bin-Name": name,
+        },
+        json=[],
+        timeout=15,
+    )
+    print(f"{name}: {r.json()['metadata']['id']}")
